15f5be5dc5340ac3dbdb890b0802dc0d7e686f7e
Florian Bruggisser <florian@nexpose.ch>
2017-06-05 03:16:18 +0200

    implemented basic version of the name generator

--- main.c
+++ main.c
@@ -1,8 +1,75 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+
+const char *colors[] = {"blue",
+                        "red",
+                        "green",
+                        "purple",
+                        "orange",
+                        "yellow",
+                        "gold",
+                        "pink",
+                        "white",
+                        "black",
+                        "silver",
+                        "gray",
+                        "teal",
+                        "aqua",
+                        "violet",
+                        "brown"};
+
+
+const char *vegetables[] = {"potato",
+                           "sweetcorn",
+                           "broccoli",
+                           "tomato",
+                           "carrot",
+                           "bean",
+                           "onion",
+                           "pepper",
+                           "pumpkin",
+                           "lettuce",
+                           "cabbage",
+                           "celery",
+                           "spinach",
+                           "cucumber",
+                           "leek",
+                           "pea"};
+
+int subStrToInt(char *str, int position)
+{
+    char hexNumber[2];
+    hexNumber[0] = str[position];
+    hexNumber[1] = '\0';
+    return (int)strtol(hexNumber, NULL, 16);
+}
+
+const char * concatStr(char *str1, char *str2, char *str3)
+{
+    char * str = (char *) malloc(1 + strlen(str1)+ strlen(str2) + strlen(str3));
+    strcpy(str, str1);
+    strcat(str, str2);
+    strcat(str, str3);
+    return str;
+}
+
+const char * generateName(char *macAddress)
+{
+    int colorIndex = subStrToInt(macAddress, 12);
+    int vegetableIndex = subStrToInt(macAddress, 13);
+
+    return concatStr((char *) colors[colorIndex], "-", (char *) vegetables[vegetableIndex]);
+}
 
 int main() {
-    char *c = "60:01:94:20:1E:0F";
+    char *macAddress = "60:01:94:20:A8:0F";
 
-    printf("Hello, World!\n");
+    printf("Mac To Name!\n");
+
+    const char* name = generateName(macAddress);
+    printf("%s", name);
+
     return 0;
 }
+
